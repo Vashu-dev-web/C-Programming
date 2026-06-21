b7ee21daef4214c9771e2ccd3e048b8f620ef0f4
Vashu <vashuantaldhaliyan@gmail.com>
2026-06-21 21:04:29 +0530

    Added Even Odd Number Program

---
+++ Conditionals/Conditional_05.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("The given number is even");
+    }
+    else
+    {
+        printf("The given number is odd");
+    }
+
+    return 0;
+}
